0c69c2e0345e366d0d5bde45024ef317d8e8933b
Atirat <wajanasoontorn@gmail.com>
2017-05-01 11:27:48 +0700

    Change to MPAndroidChart

--- app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -1,40 +1,40 @@
 package com.udacity.stockhawk.ui;
 
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.utils.EntryXComparator;
 import com.udacity.stockhawk.R;
-import com.udacity.stockhawk.utils.DateUtils;
 import com.udacity.stockhawk.data.Contract;
+import com.udacity.stockhawk.utils.DateUtils;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import lecho.lib.hellocharts.model.Axis;
-import lecho.lib.hellocharts.model.AxisValue;
-import lecho.lib.hellocharts.model.Line;
-import lecho.lib.hellocharts.model.LineChartData;
-import lecho.lib.hellocharts.model.PointValue;
-import lecho.lib.hellocharts.view.LineChartView;
 
 public class DetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     @SuppressWarnings("WeakerAccess")
     @BindView(R.id.chart)
-    LineChartView mChart;
+    LineChart mChart;
 
     private static final int STOCK_LOADER = 0;
     private String mSymbol;
@@ -77,10 +77,10 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
         }
     }
 
-    private List<PointValue> getPointValues(String raw) {
+    private List<Entry> getPointValues(String raw) {
         final int dateColumn = 0;
         final int priceColumn = 1;
-        List<PointValue> values = new ArrayList<>();
+        List<Entry> values = new ArrayList<>();
 
         try {
             String rows[] = raw.split("\\r?\\n");
@@ -88,78 +88,48 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
             for (int i = 0; i < rows.length; i++) {
                 String columns[] = rows[i].split("\\s*,\\s*");
                 Date date = new Date(Long.parseLong(columns[dateColumn]));
-                PointValue pointValue = new PointValue(date.getTime(), Float.parseFloat(columns[priceColumn]));
+                Entry pointValue = new Entry(date.getTime(), Float.parseFloat(columns[priceColumn]));
                 values.add(pointValue);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        Collections.sort(values, new Comparator<PointValue>() {
-            @Override
-            public int compare(PointValue o1, PointValue o2) {
-                if (o1.getX() > o2.getX()) {
-                    return 1;
-                } else if (o1.getX() < o2.getX()){
-                    return -1;
-                } else {
-                    return 0;
-                }
-            }
-        });
+        Collections.sort(values, new EntryXComparator());
 
         return values;
     }
 
-    private void setChart(List<PointValue> values) {
-        PointValue min = values.get(0);
-        PointValue max = values.get(values.size() - 1);
-
-        Calendar minDate = Calendar.getInstance();
-        Calendar maxDate = Calendar.getInstance();
-        minDate.setTimeInMillis((long) min.getX());
-        maxDate.setTimeInMillis((long) max.getX());
-
-        Line line = new Line(values).setColor(ContextCompat.getColor(this, R.color.chart_line)).setCubic(true);
-        line.setHasLines(true);
-        line.setHasPoints(false);
-
-        List<Line> lines = new ArrayList<>();
-        lines.add(line);
-
-        LineChartData lineChartData = new LineChartData();
-        lineChartData.setLines(lines);
-
-        Axis axisX = new Axis();
-        Axis axisY = new Axis();
-
-        int length = DateUtils.getMonthsDifference(minDate, maxDate);
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(minDate.get(Calendar.YEAR), minDate.get(Calendar.MONTH), minDate.get(Calendar.DATE));
-
-        SimpleDateFormat format = new SimpleDateFormat(DateUtils.getYearAndMonthFormat(), Locale.getDefault());
-        calendar.set(minDate.get(Calendar.YEAR), minDate.get(Calendar.MONTH), 1);
-        List<AxisValue> axisValues = new ArrayList<>();
-
-        for (int i = 0; i < length; i++) {
-            calendar.add(Calendar.MONTH, i == 0 ? 0 : 1);
-            Date date = calendar.getTime();
-            axisValues.add(new AxisValue(date.getTime()).setLabel(format.format(date)));
-        }
-
-        axisX.setValues(axisValues);
-
-        lineChartData.setAxisXBottom(axisX);
-        lineChartData.setAxisYLeft(axisY);
-
-
-        mChart.setLineChartData(lineChartData);
+    private void setChart(List<Entry> values) {
         mChart.setContentDescription(String.format(getString(R.string.a11y_price_over_time), mSymbol));
+        mChart.getDescription().setEnabled(false);
+        LineDataSet dataSet = new LineDataSet(values, "");
+        mChart.getLegend().setEnabled(false);
+        dataSet.setColor(Color.YELLOW);
+        dataSet.setDrawCircles(false);
+
+        LineData lineData = new LineData(dataSet);
+        mChart.setData(lineData);
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                SimpleDateFormat format = new SimpleDateFormat(DateUtils.getYearAndMonthFormat(), Locale.getDefault());
+                return format.format(new Date((long)value));
+            }
+        });
+
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(Color.WHITE);
+        mChart.getAxisLeft().setTextColor(Color.WHITE);
+        mChart.getAxisRight().setEnabled(false);
+
+        mChart.invalidate();
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-
+        //do nothing
     }
 }
